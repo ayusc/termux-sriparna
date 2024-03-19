cbf914238bbf26ec5ab8f5ba098d1d55f5bf935e
Ayus Chatterjee <144832717+ayusc@users.noreply.github.com>
2024-03-19 23:00:25 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,16 @@
 
 pkg update &>/dev/null && pkg upgrade -y &>/dev/null
 
+# Workaround to check for Termux Api App Installation 
+if timeout 5 termux-toast "Working ..." &>/dev/null; then
+        echo "Termux API app is already installed."
+else
+        echo "Termux API app is not installed."
+        # Open F-Droid link
+        termux-open-url "https://f-droid.org/packages/com.termux.api/"       
+        exit 1
+fi
+
 # List of packages to install
 packages=("termux-api" "python" "openssl" "libexpat")
 
