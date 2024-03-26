f91a3a35b47679a5b7af7ddcd660749f285d4380
Ayus Chatterjee <144832717+ayusc@users.noreply.github.com>
2024-03-26 12:58:49 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-widget_needed() {
-    local question="$1"
-    local response
-
-    while true; do
-        read -p "$question (yes/no): " response
-        case $response in
-            [Yy]|[Yy][Ee][Ss]) return 0 ;;  # Return success for "yes" responses
-            [Nn]|[Nn][Oo]) return 1 ;;      # Return failure for "no" responses
-            *) echo "Please answer yes or no." ;;
-        esac
-    done
-}
-
 pkg update -y && pkg upgrade -y && pkg install termux-api -y &>/dev/null
 
 # Workaround to check for Termux Api App Installation 
@@ -39,28 +25,26 @@ for pkg in "${packages[@]}"; do
 done
 
 # Termux:Widget implementation 
+echo 
+echo "Creating shortcut widgets ..."
 echo
-if widget_needed "Do you want a shortcut widget for the "; then
-    mkdir -p ~/.termux/widget/dynamic_shortcuts
-    mkdir -p /data/data/com.termux/files/home/.shortcuts
-    chmod 700 -R /data/data/com.termux/files/home/.shortcuts
-    cp sriparna.sh /data/data/com.termux/files/home/.shortcuts
-    echo "sriparna" > /data/data/com.termux/files/home/.shortcuts/Sriparna
-    echo "sriparna-gui" > /data/data/com.termux/files/home/.shortcuts/Sriparna-Gui
-    chmod +x /data/data/com.termux/files/home/.shortcuts/Sriparna
-    chmod +x /data/data/com.termux/files/home/.shortcuts/Sriparna-Gui
-    mkdir -p /data/data/com.termux/files/home/.shortcuts/icons
-    chmod -R a-x,u=rwX,go-rwx /data/data/com.termux/files/home/.shortcuts/icons
-    cp logo.png /data/data/com.termux/files/home/.shortcuts/icons/Sriparna.png
-    cp logo.png /data/data/com.termux/files/home/.shortcuts/icons/Sriparna-Gui.png
-    cp Sriparna ~/.termux/widget/dynamic_shortcuts
-    cp Sriparna-Gui ~/.termux/widget/dynamic_shortcuts
-    echo
-    echo -e "Shortcut widget is created!\nLong click on Termux:Widget app and select Termux shortcut\nThere you will get two scripts, drag them to your homescreen\nYou can directly run these scripts by clicking on them !"
-else
-    echo "Fine, widget will not be created !"
-fi
-
+mkdir -p ~/.termux/widget/dynamic_shortcuts
+mkdir -p /data/data/com.termux/files/home/.shortcuts
+chmod 700 -R /data/data/com.termux/files/home/.shortcuts
+cp sriparna.sh /data/data/com.termux/files/home/.shortcuts
+echo "sriparna" > /data/data/com.termux/files/home/.shortcuts/Sriparna
+echo "sriparna-gui" > /data/data/com.termux/files/home/.shortcuts/Sriparna-Gui
+chmod +x /data/data/com.termux/files/home/.shortcuts/Sriparna
+chmod +x /data/data/com.termux/files/home/.shortcuts/Sriparna-Gui
+mkdir -p /data/data/com.termux/files/home/.shortcuts/icons
+chmod -R a-x,u=rwX,go-rwx /data/data/com.termux/files/home/.shortcuts/icons
+cp logo.png /data/data/com.termux/files/home/.shortcuts/icons/Sriparna.png
+cp logo.png /data/data/com.termux/files/home/.shortcuts/icons/Sriparna-Gui.png
+cp /data/data/com.termux/files/home/.shortcuts/Sriparna ~/.termux/widget/dynamic_shortcuts
+cp /data/data/com.termux/files/home/.shortcuts/Sriparna-Gui ~/.termux/widget/dynamic_shortcuts
+echo
+echo -e "Shortcut widget is created!\nLong click on Termux:Widget app and select Termux shortcut\nThere you will get two scripts, drag them to your homescreen\nYou can directly run these scripts by clicking on them !"
+echo "Fine, widget will not be created !"
 echo
 echo "Setup script is successful !"
 echo 
