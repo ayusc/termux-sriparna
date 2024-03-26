d602644cc3d14199171cd1050818b7a77bced422
Ayus Chatterjee <144832717+ayusc@users.noreply.github.com>
2024-03-26 21:07:57 +0530

    Update sriparna.py

--- sriparna/sriparna.py
+++ sriparna/sriparna.py
@@ -3,6 +3,7 @@
 
 # This source code is licensed under the GPL-3.0 license found in the
 # LICENSE file in the root directory of this source tree.
+# If you don't have the source tree alternately this file can be found at <https://github.com/ayusc/termux-sriparna>
 
 import os
 import re
