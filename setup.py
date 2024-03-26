da09716df5c1e1544d46d6a30b9febf3a4783d1c
Ayus Chatterjee <144832717+ayusc@users.noreply.github.com>
2024-03-26 21:10:02 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
+# Copyright (c) 2024, Ayus Chatterjee
+# All rights reserved.
+
+# This source code is licensed under the GPL-3.0 license found in the
+# LICENSE file in the root directory of this source tree.
+# If you don't have the source tree alternately this file can be found at <https://github.com/ayusc/termux-sriparna>
+
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 import subprocess
