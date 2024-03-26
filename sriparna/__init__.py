5893af9666cbf3ddd0c5349ae661ea41a07fc48b
Ayus Chatterjee <144832717+ayusc@users.noreply.github.com>
2024-03-26 21:00:40 +0530

    Update __init__.py

--- sriparna/__init__.py
+++ sriparna/__init__.py
@@ -1,1 +1,5 @@
-# Initialise the package
+# Copyright (c) 2024, Ayus Chatterjee
+# All rights reserved.
+
+# This source code is licensed under the GPL-3.0 license found in the
+# LICENSE file in the root directory of this source tree.
