c384d2c6aba141450becc5c4b432af52ac23b006
Ayus Chatterjee <144832717+ayusc@users.noreply.github.com>
2024-03-26 21:09:00 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Copyright (c) 2024, Ayus Chatterjee
+# All rights reserved.
+
+# This source code is licensed under the GPL-3.0 license found in the
+# LICENSE file in the root directory of this source tree.
+# If you don't have the source tree alternately this file can be found at <https://github.com/ayusc/termux-sriparna>
+
 yes no | pkg update -y 
 yes no | pkg upgrade -y 
 yes no | pkg install termux-api -y
